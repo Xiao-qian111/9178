40243dbaf0292b0d3a9deeec528f3d0efd757096
SCXG <144230175+Xiao-qian111@users.noreply.github.com>
2026-09-19 11:15:22 +0800

    Update header and output formatting in main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ number = [
     r"(91-78)-(\sqrt{9\times1}-(7-8))",    # 9
     r"((7+8)-\sqrt{9}+1)-\abs{(78+(-91))+(9+1)}"    # 10
 ]
-st.header("9178生成器", text_alignment="center")
-st.subtitle("未满18岁的用户可以使用，因为SCXG是给", text_alignment="center")
+st.title("9178生成器", text_alignment="center")
+st.subheader("未满18岁的用户可以使用，因为SCXG是给", text_alignment="center")
 need = str(st.number_input("输入1~99999999999之间的整数：", min_value=1, max_value=99999999999, step=1))
 gogogo = st.button("生成算式")
 if gogogo:
@@ -26,4 +26,4 @@ if gogogo:
             ans += r"+"
         index -= 1
     ans = need + r"=" + ans
-    emp.title(ans, text_alignment="center")
+    emp.latex(ans)
